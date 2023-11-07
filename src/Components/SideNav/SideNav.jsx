53e774f7df24899ee8c640d4a78afd1049a46717
rohan future devoloper <xrohaislam373@gmail.com>
2023-11-07 20:36:36 +0600

    social icon

--- src/Components/SideNav/SideNav.jsx
+++ src/Components/SideNav/SideNav.jsx
@@ -62,21 +62,36 @@ const SideNav = () => {
 
       <div className="md:block hidden fixed top-[25%] z-10 ">
         <div className="flex flex-col">
-            <a href="#main" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 ">
-                <AiOutlineHome size={20}></AiOutlineHome>
-            </a>
-            <a href="#work" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 ">
-                <GrProjects size={20}></GrProjects>
-            </a>
-            <a href="#projects" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 ">
-                <AiOutlineProject size={20}></AiOutlineProject>
-            </a>
-            <a href="#main" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 ">
-                <BsPerson size={20}></BsPerson>
-            </a>
-            <a href="#contact" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 ">
-                <AiOutlineMail size={20}></AiOutlineMail>
-            </a>
+          <a
+            href="#main"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 "
+          >
+            <AiOutlineHome size={20}></AiOutlineHome>
+          </a>
+          <a
+            href="#work"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 "
+          >
+            <GrProjects size={20}></GrProjects>
+          </a>
+          <a
+            href="#projects"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 "
+          >
+            <AiOutlineProject size={20}></AiOutlineProject>
+          </a>
+          <a
+            href="#main"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 "
+          >
+            <BsPerson size={20}></BsPerson>
+          </a>
+          <a
+            href="#contact"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 "
+          >
+            <AiOutlineMail size={20}></AiOutlineMail>
+          </a>
         </div>
       </div>
     </div>
--- src/Page/home/Home/Home.jsx
+++ src/Page/home/Home/Home.jsx
@@ -1,9 +1,46 @@
+import { TypeAnimation } from "react-type-animation";
+import img1 from "../../../photo/343617328_1317960939123788_1840975766021119825_n-removebg-preview.png";
+import {  FaFacebook, FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
 const Home = () => {
-    return (
+  return (
+    <div id="main">
+      <img
+        className="w-full h-screen object-cover object-left"
+        src={img1}
+        alt=""
+      />
+      <div className="w-full h-screen absolute top-0  left-0 bg-white/50">
+        <h1>I'm Rohan IsLam</h1>
+        <h2>
+          <TypeAnimation
+            sequence={[
+              "I'm a MERN-Stack Developer",
+              1000,
+              "Coder",
+              1000,
+              "Enthusiast",
+              1000,
+            ]}
+            wrapper="span"
+            speed={50}
+            style={{
+              fontSize: "2em",
+              paddingLeft: "5px",
+              display: "inline-block",
+            }}
+            cursor={true}
+            repeat={Infinity}
+          />
+        </h2>
         <div>
+            <FaFacebook></FaFacebook>
+            <FaInstagram></FaInstagram>
+            <FaLinkedinIn></FaLinkedinIn>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
